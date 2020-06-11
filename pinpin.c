e4430ac5ba1f3eeea77fe73f464d1cf6c0109490
lea oxaran <oxaran.lea@gmail.com>
2020-06-11 15:32:27 +0200

    Ma petite touche perso

--- pinpin.c
+++ pinpin.c
@@ -1,1 +1,8 @@
-petit lapin
+petit lapin blanc
+si jamais j'apprend
+que par une belle matinée
+l'envie t'es venue de croquer
+dans les fleurs du potager
+je peut t'assurer
+que tu t'en souviendra
+jusqu'au jour de ton trépas
